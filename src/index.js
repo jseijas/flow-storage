f2f29f5e2fe5eda02308a1a37f43e0b54865fd5e
Jesus Seijas <jseijas@gmail.com>
2017-02-21 00:09:07 +0100

    Added a Loopback Collection.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import BaseCollection from './base-collection';
 import MemoryCollection from './memory-collection';
 import JsonCollection from './json-collection';
 import FlowStorage from './flow-storage';
+import LoopbackCollection from './loopback-collection';
 
 export { BaseCollection };
 export { MemoryCollection };
 export { JsonCollection };
 export { FlowStorage };
+export { LoopbackCollection };
